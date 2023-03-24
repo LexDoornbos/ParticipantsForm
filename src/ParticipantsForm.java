54f7930cb68255c24351f394444fcbd223af832c
Lex Doornbos <lex@codegorilla.nl>
2023-03-24 10:54:18 +0100

    commit 1

---
+++ src/ParticipantsForm.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ParticipantsForm {
+    private JPanel panel;
+    private JTextField textField1;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JTextArea textArea1;
+    private JButton addParticipantButton;
+    private JTextPane textPane1;
+public ParticipantsForm() {
+    addParticipantButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+}
