b962cbb5bd2736ede8b688bab6a5f74529862f34
Lex Doornbos <lex@codegorilla.nl>
2023-03-27 15:37:32 +0200

    commit 1

--- src/ParticipantsForm.java
+++ src/ParticipantsForm.java
@@ -1,21 +1,52 @@
 import javax.swing.*;
+import javax.xml.namespace.QName;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
 
-public class ParticipantsForm {
-    private JPanel panel;
-    private JTextField textField1;
-    private JSpinner spinner1;
-    private JSpinner spinner2;
-    private JTextArea textArea1;
-    private JButton addParticipantButton;
-    private JTextPane textPane1;
-public ParticipantsForm() {
-    addParticipantButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+public class ParticipantsForm extends JFrame {
+    private JPanel panelMain;
+    private JTextField txtName;
+    private JButton btnClick;
+    private JSpinner hourSpinner;
+    private JSpinner minuteSpinner;
+    private JTextArea statusArea;
+    private JTextArea listArea;
 
-        }
-    });
-}
+
+    public ParticipantsForm() {
+        // set up the hour and minute spinner
+        SpinnerNumberModel hourModel = new SpinnerNumberModel(0, 0 ,23, 1);
+        hourSpinner.setModel(hourModel);
+        SpinnerNumberModel minuteModel = new SpinnerNumberModel(0, 0 ,59, 1);
+        minuteSpinner.setModel(minuteModel);
+        Calendar cal = Calendar.getInstance();
+
+        btnClick.addActionListener(e -> {
+            String name = txtName.getText();
+            String timeSlot = String.format("%02d", hourSpinner.getValue()) + ":" + String.format("%02d", minuteSpinner.getValue());
+
+            if (name.equals("Enter name:") || name.equals("")) {
+                statusArea.append("The given name is not valid.\n");
+            } else {
+                statusArea.append("The participant was added.\n");
+                listArea.append(name + " on time " + timeSlot + "\n");
+            }
+            txtName.setText("Enter name:");
+            hourModel.setValue(12);
+            minuteModel.setValue(30);
+        });
+    }
+
+    public static void main(String[] args) {
+        ParticipantsForm h = new ParticipantsForm();
+        h.setContentPane(h.panelMain);
+        h.setTitle("Participants Form");
+        h.setBounds(600, 200, 600, 600);
+        h.setVisible(true);
+        h.statusArea.setEditable(false);
+        h.listArea.setEditable(false);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        h.txtName.setText("Enter name:");
+    }
 }
