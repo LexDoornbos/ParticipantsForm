0d31e3b19b3081e932cd38b0d5818d4271bcf4c9
Lex Doornbos <lex@codegorilla.nl>
2023-03-30 06:11:58 +0200

    commit 4 - With a little help from ChatGPT for the extra external textfile functionality

--- src/ParticipantsForm.java
+++ src/ParticipantsForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ParticipantsForm extends JFrame {
     private JPanel panelMain;
@@ -12,27 +14,42 @@ public class ParticipantsForm extends JFrame {
 
 //
     public ParticipantsForm() {
-        // window title
-        setTitle("Participants Form");
-        // set position and size of window
-        setBounds(600, 200, 600, 600);
+        setTitle("Participants Form");  // window title
+        setBounds(600, 200, 600, 600);  // set position and size of window
         setVisible(true);
         setContentPane(panelMain);
-        // make text-area's non-editable
-        statusArea.setEditable(false);
+        statusArea.setEditable(false);  // make text-area's non-editable
         listArea.setEditable(false);
-        // set initial textField content
-        txtName.setText("Enter name:");
+        txtName.setText("Enter name:");  // set initial textField content
         // set up the hour and minute spinner with proper constraints
-        SpinnerNumberModel hourModel = new SpinnerNumberModel(0, 0 ,23, 1);
+        SpinnerNumberModel hourModel = new SpinnerNumberModel(12, 0 ,23, 1);
         hourSpinner.setModel(hourModel);
-        SpinnerNumberModel minuteModel = new SpinnerNumberModel(0, 0 ,59, 1);
+        SpinnerNumberModel minuteModel = new SpinnerNumberModel(30, 0 ,59, 1);
         minuteSpinner.setModel(minuteModel);
-        // make program exit when window is closed
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // make program exit when window is closed
 
         // initialize an empty ArrayList for storing Participants
-        ArrayList<Participant> participants = new ArrayList<>();
+        ArrayList<String> participantsList = new ArrayList<>();
+        // Arraylist for checking available time slots
+        ArrayList<String> reservedTimeSlots = new ArrayList<>();
+
+        // read the contents of the text file and populate the text area.. (Thank you ChatGPT)
+        try {
+            File file = new File("participants.txt");
+            if (file.exists()) {
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    participantsList.add(line);
+                    reservedTimeSlots.add(line.substring(line.lastIndexOf(" ") + 1));
+                }
+                scanner.close();
+                // display the contents of the participantsList array in the text area
+                listArea.setText(String.join("\n", participantsList));
+            }
+        } catch (FileNotFoundException e) {
+            statusArea.append("File not found: participants.txt\n");
+        }
 
         // what happens on btnClick
         btnClick.addActionListener(e -> {
@@ -43,22 +60,38 @@ public class ParticipantsForm extends JFrame {
             // check if name entered is not empty or still at default value
             if (name.equals("Enter name:") || name.equals("")) {
                 statusArea.append("The given name is not valid.\n");
+            // check if the time slot is already reserved
+            } else if (reservedTimeSlots.contains(timeSlot)) {
+                statusArea.append("Time slot taken. Please choose another.\n");
             } else {
                 // create new instance of Participant class and assign entered name and time slot
                 Participant participant = new Participant(name, timeSlot);
-                // add to ArrayList participants
-                participants.add(participant);
-                System.out.println(participants);
+                // add to ArrayList participants and reservedTimeSlots
+                participantsList.add(participant.getName() + " on time " + participant.getTimeSlot());
+                reservedTimeSlots.add(participant.getTimeSlot());
                 statusArea.append("The participant was added.\n");
-                listArea.append(participant.getName() + " on time " + participant.getTimeSlot() + "\n");
+                // clear listArea before appending participantsList
+                listArea.setText("");
+                listArea.append(String.join("\n", participantsList));
+                // append the new participant to text file.. (Thank you ChatGPT)
+                try {
+                    // open the text file for appending
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("participants.txt", true));
+                    writer.write(participant.getName() + " on time " + participant.getTimeSlot() + "\n");
+                    writer.close();
+                } catch (IOException ex) {
+                    // if there's an error writing to the text file, just print an error message and continue
+                    System.err.println("Error writing to participants.txt: " + ex.getMessage());
+                }
             }
+            // reset input fields to chosen defaults
             txtName.setText("Enter name:");
             hourModel.setValue(12);
             minuteModel.setValue(30);
         });
     }
 
-    public static class Participant {
+    public class Participant {
         private String name;
         private String timeSlot;
 
@@ -75,6 +108,7 @@ public class ParticipantsForm extends JFrame {
             return timeSlot;
         }
     }
+
 
     public static void main(String[] args) {
         // calling new instance of constructor of class ParticipantsForm
